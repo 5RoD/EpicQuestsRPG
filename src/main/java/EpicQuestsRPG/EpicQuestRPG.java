3dd776a4df473f4b4586d47a6875330d026564b6
Mo F <5RoD@gravemc.net>
2024-12-02 01:25:16 +0100

    First Commit

---
+++ src/main/java/EpicQuestsRPG/EpicQuestRPG.java
@@ -0,0 +1,17 @@
+package EpicQuestsRPG;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EpicQuestRPG extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
