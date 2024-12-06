e158cf8478a1a39ae9f91ebadd60cb8f622e7694
Mo <5RoD@gravemc.net>
2024-12-06 11:31:35 +0100

    Trying new stuff with this gui framework

--- src/main/java/EpicQuestsRPG/gui/MenuUtil.java
+++ src/main/java/EpicQuestsRPG/gui/MenuUtil.java
@@ -16,11 +16,32 @@ public class MenuUtil {
                 .rows(6)
                 .create();
 
-        GuiItem guiItem = ItemBuilder.from(Material.STONE).asGuiItem(event -> {
+        GuiItem filler = ItemBuilder.from(Material.GRAY_STAINED_GLASS_PANE).asGuiItem(event -> {
+            event.setCancelled(true);
+
+        });
+
+        GuiItem guiItem = ItemBuilder.from(Material.SHULKER_BOX).asGuiItem(event -> {
+            event.setCancelled(true);
             player.sendMessage(CC.translate("You clicked me hehe"));
         });
 
-        gui.addItem(guiItem);
+
+        GuiItem guiItem2 = ItemBuilder.from(Material.BARREL).asGuiItem(event -> {
+            event.setCancelled(true);
+            player.sendMessage(CC.translate("You clicked me hehe"));
+        });
+
+        GuiItem guiItem3 = ItemBuilder.from(Material.BARRIER).asGuiItem(event -> {
+            event.setCancelled(true);
+            player.sendMessage(CC.translate("You closed the gui"));
+            gui.close(player);
+        });
+
+        gui.getFiller().fill(filler);
+        gui.setItem(21, guiItem);
+        gui.setItem(22, guiItem2);
+        gui.setItem(23, guiItem3);
         gui.open(player);
     }
 }
